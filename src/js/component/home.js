b51dba41ae55b84c9ddd630759b01e170700ea73
eleanablandin <eleanablandin@gmail.com>
2021-05-21 00:59:49 -0400

    Lista de tareas utilizando react, useState

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,64 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { event } from "jquery";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [newTask, setNewTask] = useState("");
+	const [tasks, setTasks] = useState([]);
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center mt-5 d-flex align-items-center flex-column">
+			<h1>TO-DO LIST</h1>
+			<div className="boxList">
+				<input
+					className="form-control"
+					type="text"
+					placeholder="Add a New Task"
+					value={newTask}
+					onChange={event => setNewTask(event.target.value)}
+					onKeyPress={event => {
+						if (event.key == "Enter") {
+							setTasks([
+								...tasks,
+								{
+									id: Math.random()
+										.toString(16)
+										.substring(2),
+									label: newTask
+								}
+							]);
+							setNewTask("");
+						}
+					}}
+				/>
+
+				<ul className="p-0">
+					{tasks.map(task => (
+						<li key={task.id}>
+							{task.label}
+							<span
+								onClick={event => {
+									let filterTasks = tasks.filter(
+										t => t.id != task.id
+									);
+									setTasks(filterTasks);
+								}}>
+								<i className="far fa-times-circle"></i>
+							</span>
+						</li>
+					))}
+				</ul>
+				<div>
+					<span>
+						{tasks.length == 1
+							? `${tasks.length} pending tasks`
+							: tasks.length == 0
+							? "You dont have tasks"
+							: `${tasks.length} pending tasks`}
+					</span>
+				</div>
+			</div>
 		</div>
 	);
 }
